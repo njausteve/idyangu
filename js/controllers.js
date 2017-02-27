e3958a17da23c73dfafc09633a5c8ed669328270
njaustevedomino <njaustevedomino@gmail.com>
2017-02-27 11:29:13 -0500

    Added views Angular folder structure

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,61 @@ angular.module('idyangu.controllers', [])
  
  function($scope ,$state, $stateParams) {
    
-   
          console.log("inside mainCtlr");
-
+   
+            
+              $scope.searchCall = function(itemId, itemType){
+                        
+                        $scope.itemType = itemType;
+                        $scope.itemId = itemId;
+                
+                        console.log('clicked search ' + itemType  + ' : ' + itemId);
+                
+                
+                      $state.go('search', {itemid: itemId, itemtype: itemType} );
+                  
+              } 
+   
+   
                  $scope.me = "passports";
                  $scope.hello = "About"; 
 
               }
 ])
+
+
+.controller('searchCtlr', ['$scope', '$state', '$stateParams', 'idyangservice', 
+ 
+ function($scope ,$state, $stateParams) {
+          
+          $scope.itemid = $stateParams.itemtype;
+          $scope.itemtype = $stateParams.itemid;
+   
+   
+     
+         console.log("inside searchCtlr" );
+   
+              }
+])
+
+
+
+.controller('navbarCtlr', ['$scope', '$state', '$stateParams', 'idyangservice', 
+ 
+ function($scope ,$state, $stateParams) {
+   
+   
+    
+         console.log("inside navbarCtlr");
+   
+              }
+])
+
+
+
+
+
+
+
+
+
--- js/routes.js
+++ js/routes.js
@@ -4,19 +4,46 @@ angular.module('idyangu.routes', ['ui.router'])
   
     $stateProvider
         
-//         // HOME STATES AND NESTED VIEWS ========================================
+//         // HOME STATES AND multiple named views ========================================
         .state('home', {
             url: '/home',
-            templateUrl: './views/home.html',
-            controller: 'mainCtlr'
+      
+            views: {  '': {   templateUrl: './views/home.html',
+                               controller : 'mainCtlr' 
+                         
+                         },
+                    
+                     'homeHeader@home':{ templateUrl : './views/home-header.html',
+                                         controller : 'mainCtlr'
+                     
+                         },
+                    
+                     'homeContent@home': {  templateUrl : './views/home-content.html',
+                          
+                                             controller : 'mainCtlr'
+                        
+                         }
+               
+                   }
+          
         }) 
         
-//         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-// //         .state('about', {
-// //              url : '/about',
-// //              templateUrl: './pages/about.html',
-// //              controller: 'aboutController'    
-// //         });
+        // SEARCH PAGE AND MULTIPLE NAMED VIEWS =================================
+    
+        .state('search', {
+      
+             url : '/search/:itemtype/:itemid',
+            views: {   '@': { templateUrl: './views/home.html',
+                              controller: 'searchCtlr'
+                           },
+                    
+                       'searchResults@search': {
+                                  templateUrl: './views/search-default.html',
+                                  controller: 'searchCtlr'
+                           }
+                   
+                   }    
+        });
         
   $urlRouterProvider.otherwise('/home');  // default route
 });
